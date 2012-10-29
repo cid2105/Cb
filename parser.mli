e69f5c3cb6abffa1c4cfb95954999214bc6908d2
Marcellin Nshimiyimana <mn2587@columbia.edu>
2012-10-28 21:29:28 -0400

    lexiacal convention
    .

---
+++ parser.mli
@@ -0,0 +1,58 @@
+type token =
+  | INTLITERAL of (int)
+  | OCTAVE of (int)
+  | DURATIONINT of (int)
+  | DURATIONCONST of (string)
+  | STRING of (string)
+  | DATATYPE of (string)
+  | NOTECONST of (string)
+  | ID of (string)
+  | IN
+  | IF
+  | ELSE
+  | NOELSE
+  | WHILE
+  | FOREACH
+  | ASSIGN
+  | PLUSEQ
+  | MINUSEQ
+  | TIMESEQ
+  | DIVIDEEQ
+  | MOD
+  | MODEQ
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | IS
+  | ISNT
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | PLUSPLUS
+  | MINUSMINUS
+  | SHARP
+  | FLAT
+  | RAISE
+  | LOWER
+  | LEFTPAREN
+  | RIGHTPAREN
+  | LBRAC
+  | RBRAC
+  | INT
+  | NOTE
+  | CHORD
+  | SCALE
+  | STANZA
+  | SCORE
+  | METH
+  | RETURN
+  | END
+  | VASSIGN
+  | SEMICOLON
+  | COMMA
+  | DOT
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast_tmp.program
