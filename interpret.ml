edc05471a02c27325bb8c9cd19ab37fa91c3f448
Matthew Cowan <mpc2145@hotmail.com>
2012-12-12 17:14:55 -0500

    added type definitions to the interpreter

---
+++ interpret.ml
@@ -0,0 +1,50 @@
+open Ast
+open Printf
+
+module NameMap = Map.Make(struct
+    type t = string
+    let compare x y = Pervasives.compare x y
+end)
+
+type note = {
+    mutable pitch : int;
+    mutable octave : int;
+    mutable duration : int;
+}
+
+type chord = {
+    mutable notelist : note list;
+    mutable duration : int;
+}
+
+type scale = {
+    mutable notelist : note list;
+}
+
+(*Assumes notes in a stanza are auto-converted to chords*)
+type stanza = {
+    mutable chordlist : chord list;
+}
+
+type score = {
+    mutable stanzalist : stanza list;
+}
+
+type cbtype =   Int of int
+                | Bool of bool
+                | Note of note
+                | Chord of chord
+                | Scale of scale
+                | Stanza of stanza
+                |Score of score
+
+let getType v =
+    match v with
+        Int(v) -> "int"
+        | Bool(v) -> "bool"
+        | Note(v) -> "note"
+        | Chord(v) -> "chord"
+        | Scale(v) -> "scale"
+        | Stanza(v) -> "stanza"
+        | Score(v) -> "score"
+
