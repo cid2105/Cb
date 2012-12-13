72638e7cd3342e78c174de61a4552984f24618b5
Matthew Cowan <mpc2145@hotmail.com>
2012-12-13 01:40:41 -0500

    methodized the program

--- Cb.ml
+++ Cb.ml
@@ -1,4 +1,5 @@
 let _ =
   let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
+  let methodized = "meth void program()\n" ^ lexbuf ^ "\nend\n"
+  let program = Parser.program Scanner.token methdized in
   ignore (Interpret.run program)
