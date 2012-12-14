2639a19255cd05c48d47f57a93b7d7f64c6aff81
Matthew Cowan <mpc2145@hotmail.com>
2012-12-14 00:07:29 -0500

    removed more deadspace

--- ast.ml
+++ ast.ml
@@ -157,7 +157,6 @@ let rec string_of_stmt = function
                     Stmt2(st) -> string_of_stmt st ^ "\n";
                     | VDecl2(v) -> string_of_vdecl v ^ "\n" ;
                     | FullDecl2(fv) -> string_of_fvdl fv ^ "\n" ;
-
                 )
              s);(* no elsif part given *)
             | _ -> "if (" ^ string_of_expr e ^ ")\n" ^ String.concat "\n" (List.map
@@ -167,7 +166,6 @@ let rec string_of_stmt = function
                     Stmt2(st) -> string_of_stmt st ^ "\n";
                     | VDecl2(v) -> string_of_vdecl v ^ "\n" ;
                     | FullDecl2(fv) -> string_of_fvdl fv ^ "\n" ;
-
                 )
 
             s) ^ "\n" ^ string_of_stmt els;
@@ -175,55 +173,42 @@ let rec string_of_stmt = function
     | If(e, s1, els, s2) -> begin
         match els with
             Block([]) ->    "if (" ^ string_of_expr e ^ ")\n" ^ String.concat "\n" (List.map
-
                  (fun innerb ->
                     match innerb with
                     Stmt2(st) -> string_of_stmt st ^ "\n";
                     | VDecl2(v) -> string_of_vdecl v ^ "\n" ;
                     | FullDecl2(fv) -> string_of_fvdl fv ^ "\n" ;
-
                 )
-
-
              s1) ^
                             "else\n" ^ string_of_stmt s2;
             | _ ->  "if (" ^ string_of_expr e ^ ")\n" ^ String.concat "\n" (List.map
-
                  (fun innerb ->
                     match innerb with
                     Stmt2(st) -> string_of_stmt st ^ "\n";
                     | VDecl2(v) -> string_of_vdecl v ^ "\n" ;
                     | FullDecl2(fv) -> string_of_fvdl fv ^ "\n" ;
-
                 )
 
 
              s1) ^ "\n" ^ string_of_stmt els ^
                     "else\n" ^ string_of_stmt s2; end
     | ElseIf(e, els) ->  "elsif (" ^ string_of_expr e ^ ")\n" ^ String.concat "\n" (List.map
-
          (fun innerb ->
                     match innerb with
                     Stmt2(st) -> string_of_stmt st ^ "\n";
                     | VDecl2(v) -> string_of_vdecl v ^ "\n" ;
                     | FullDecl2(fv) -> string_of_fvdl fv ^ "\n" ;
-
                 )
-
 
      els)
     | Foreach(p, id, s) ->
         "foreach (" ^ string_of_pdecl p  ^ " in " ^ id ^ " )\n" ^ String.concat "\n" (List.map
-
              (fun innerb ->
                     match innerb with
                     Stmt2(st) -> string_of_stmt st ^ "\n";
                     | VDecl2(v) -> string_of_vdecl v ^ "\n" ;
                     | FullDecl2(fv) -> string_of_fvdl fv ^ "\n" ;
-
                 )
-
-
          s);
 
     | While(e, s) -> "while (" ^ string_of_expr e ^ ")\n" ^ String.concat "\n" (List.map
