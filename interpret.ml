87993262d6192f79c201954739d4deec4a7efd1a
Matthew Cowan <mpc2145@hotmail.com>
2012-12-13 21:17:18 -0500

    updating

--- interpret.ml
+++ interpret.ml
@@ -48,7 +48,7 @@ let getInt v =
         Int(v) -> v
         | _ -> 0
 
-let getBool v = 
+let getBool v =
     match v with
         Bool(v) -> v
         | _ -> false
@@ -130,10 +130,10 @@ let rec eval env = function
     | NoteConst(s) -> print_string ("I am a note constant: " ^ s ^ "\n");
         Int (NameMap.find s noteMap), env
     | BoolLiteral(b) -> print_string ("I am a bool literal: " ^ (string_of_bool b) ^ "\n"); (Bool b, env)
-   (* | Assign(toE, fromE) -> print_string ("I am an assignment\n")
-    | NoteExpr(s,e,e1) -> print_string ("I am a note expression: " ^ s ^ "," ^ "\n")
-    | ChordExpr(el, e) -> print_string ("I am a chord expression: \n")
-    | ListExpr([el]) -> print_string ("I am a list epxression\n") *)
+   (* | Assign(toE, fromE) -> print_string ("I am an assignment\n") *)
+    (*| NoteExpr(s,e,e1) -> print_string ("I am a note expression: " ^ s ^ "," ^ "\n") *)
+    (* | ChordExpr(el, e) -> print_string ("I am a chord expression: \n") *)
+    (* | ListExpr([el]) -> print_string ("I am a list epxression\n") *)
     | BinOp(e1,o,e2) ->
          let v1, env = eval env e1 in
          let v2, env = eval env e2 in
