54b3815f747cc348c05ac6c2fc911413c2d35f69
Marcellin Nshimiyimana <mn2587@columbia.edu>
2012-12-15 00:42:57 -0500

    stanzas prob

--- interpret.ml
+++ interpret.ml
@@ -349,7 +349,7 @@ let rec eval env = function
                 else raise (Failure ("compose takes a score only"))); 
 ======= *)
     (* assume you get notes only/ no error checking yet ex: [3, 4, 5] is not checked *)
-    | MethodCall("compose", [e]) -> (* Writes the specified part to a java file to be written into midi *)
+    | MethodCall("compose", [e]) -> print_string "yoohoo i got it!"; (* Writes the specified part to a java file to be written into midi *)
             ignore (match e with
                         Id(i) -> i
                         | _ ->  raise (Failure ("compose takes an identifier as input")));
@@ -359,39 +359,84 @@ let rec eval env = function
                     (let headers = csv_head in
                         let csvf = open_out "musicfi.csv" in
                             (fprintf csvf "%s" headers;
+
                             (* note a = (C, 1, half) csv format => placement(0,4,8...), duration(half), pitch(C) *)
                             let print_note nt =
-                                fprintf csvf "%s\n" ( (string_of_int !tick) ^ "," ^
-                                                    (string_of_int nt.duration) ^ "," ^
+                                fprintf csvf "%s\n" ( (string_of_int !tick) ^ "," ^ 
+                                                    (string_of_int nt.duration) ^ "," ^ 
                                                     (string_of_int nt.pitch));
-                                (tick := !tick + nt.duration );
-                            in
-                            let print_chord cd =
-                                List.iter (fun nt ->
-                                            fprintf csvf "%s\n" ( (string_of_int !tick) ^ "," ^
-                                                            (string_of_int cd.chord_duration) ^ "," ^
+
+                                (tick := !tick + nt.duration )
+                            in 
+                            let print_chord cd = 
+                                List.map (fun nt ->
+                                            fprintf csvf "%s\n" ( (string_of_int !tick) ^ "," ^ 
+                                                            (string_of_int (cd.chord_duration / 4)) ^ "," ^ 
                                                             (string_of_int nt.pitch));
                                             );
                                 (tick := !tick + cd.chord_duration);
                                        (*  let a = (List.map (fun nt -> (nt.duration <- cd.chord_duration) ) cd.notelist) in
                                         begin   print_note ((List.hd  a));
                                     end *)
+                            
                             in
                             let print_stanza stan =
-                                    if (List.length stan.chordlist = 1) then
+                                   (*  if (List.length stan.chordlist = 1) then
+                                        begin 
+                                            let nt = (List.hd stan.chordlist); 
+                                            in print_note (List.hd nt.notelist );
+                                        end
+                                    else *)
+                                        List.map  print_chord (List.rev stan.chordlist);
+                            in
+
+                        List.map print_stanza (List.rev pp.stanzalist)
+
+                    );
+
+                    close_out csvf);
+                    ee1
+                    , env
+                else raise (Failure ("compose takes a score only")));
+           (*  let ee1, env = eval env e in
+                (if getType ee1 = "score" then
+                    let pp = getScore(ee1) in
+                    (let headers = csv_head in
+                        let csvf = open_out "musicfi.csv" in
+                            (fprintf csvf "%s" headers;
+                            (* note a = (C, 1, half) csv format => placement(0,4,8...), duration(half), pitch(C) *)
+                            let print_note nt =
+                                fprintf csvf "%s\n" ( (string_of_int !tick) ^ "," ^
+                                                    (string_of_int (nt.duration / 4)) ^ "," ^
+                                                    (string_of_int nt.pitch));
+                                (tick := !tick + (nt.duration  / 4) );
+                            in
+                            let print_chord cd =
+                                List.map (fun nt ->
+                                            fprintf csvf "%s\n" ( (string_of_int !tick) ^ "," ^
+                                                            (string_of_int (cd.chord_duration / 4 )) ^ "," ^
+                                                            (string_of_int nt.pitch));
+                                            );
+                                (tick := !tick + (cd.chord_duration / 4) );
+                                       (*  let a = (List.map (fun nt -> (nt.duration <- cd.chord_duration) ) cd.notelist) in
+                                        begin   print_note ((List.hd  a));
+                                    end *)
+                            in
+                            let print_stanza stan =
+                                    (* if (List.length stan.chordlist = 1) then
                                         begin
                                             let nt = (List.hd stan.chordlist);
                                             in print_note (List.hd nt.notelist );
                                         end
-                                    else
-                                        List.iter  print_chord (List.rev stan.chordlist);
+                                    else *)
+                                        List.map  print_chord stan.chordlist;
                             in
                         List.iter print_stanza (List.rev pp.stanzalist)
                     );
                     close_out csvf);
                     ee1
                     , env
-                else raise (Failure ("compose takes a score only")));
+                else raise (Failure ("compose takes a score only"))); *)
     | MethodCall(name, el) -> print_string ("Calling Method: " ^ name ^ "\n");
         let locals, globals, fdecls = env in
             let fdecl = try (NameMap.find name fdecls)
